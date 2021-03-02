e3730f962969ed653b06e114aad2cc38e3112bd5
Alexandra Titova <auto-steklo@mail.ru>
2021-03-02 21:01:40 +0300

    map*

---
+++ src/__test__/settings.test.js
@@ -0,0 +1,23 @@
+import Settings from '../js/Settings';
+import options from '../js/options';
+
+test('добавление корректной пары опция:значение', () => {
+  const app = new Settings({
+    theme: 'gray',
+  });
+  const received = Object.fromEntries(app.settings.entries());
+  const result = {
+    theme: 'gray',
+    music: 'trance',
+    difficulty: 'easy',
+  };
+  expect(received).toStrictEqual(result);
+});
+
+test('добавление некорректной опции', () => {
+  expect(() => new Settings({ music: 'grunge' })).toThrow('несуществующее значение');
+});
+
+test('добавление некорректной опции', () => {
+  expect(() => new Settings({ balance: 100 })).toThrow('несуществующая опция');
+});
